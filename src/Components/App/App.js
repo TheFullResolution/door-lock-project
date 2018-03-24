f7819677398ed89db7a0e85ede9560072e221570
Jedrzej Lewandowski <progreacja@gmail.com>
2018-03-24 15:37:28 +0100

    Setup FireBase :fire:

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import * as style from './App.scss';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const App = () => (
+  <div className={style.container}>
+    App
+  </div>
+);
+
+App.propTypes = {
+
+};
---
+++ src/store/redux.js
@@ -0,0 +1,32 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import { reducer as formReducer } from 'redux-form'
+import firebase from 'firebase'
+
+
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyCIs_8n_ROn-WxF4ptSzAz14fI_q0Ls-yY",
+  authDomain: "door-lock-project.firebaseapp.com",
+  databaseURL: "https://door-lock-project.firebaseio.com",
+  projectId: "door-lock-project",
+}
+// react-redux-firebase options
+const config = {
+  userProfile: 'users',
+  enableLogging: false
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const rootReducer = combineReducers({ form: formReducer})
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk.withExtraArgument(getFirebase)),
+    reactReduxFirebase(firebase, config)
+  )
+)
