839002c8a1900fc0e4c60f63a9ae7eb11ac18a90
Jedrzej Lewandowski <progreacja@gmail.com>
2018-09-05 15:06:10 +0200

    Updates

--- src/helpers/classAndIds.js
+++ src/helpers/classAndIds.js
@@ -3,4 +3,4 @@ import compact from 'lodash-es/compact'
 export const combineClassName = (...classNames) =>
   compact(classNames).join(' ')
 
-export const formatId = (id: string) => id.toLowerCase().replace(/\s/g, '-')
+export const formatId = (id) => id.toLowerCase().replace(/\s/g, '-')
